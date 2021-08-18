3de5c87375e24c4e1fb680e07c9acfa2d7c5a194
Huloud <89122589+Huloud@users.noreply.github.com>
2021-08-18 14:03:40 +0800

    Add files via upload

---
+++ assets/tscs/shaders/uniformShaderConstants.h
@@ -0,0 +1,8 @@
+#include frex:shaders/lib/math.glsl
+#include frex:shaders/lib/noise/cellular2x2x2.glsl
+#include frex:shaders/lib/noise/noise2d.glsl
+#include frex:shaders/lib/noise/noise3d.glsl
+#include frex:shaders/api/material.glsl
+#include frex:shaders/api/player.glsl
+#include frex:shaders/api/view.glsl
+#include frex:shaders/api/world.glsl
